0cffc7437a9c0e86a20e88d8412092d701261467
Kelvin Hey <kelvinhey@outlook.com>
2024-06-25 19:40:02 -0300

    ✨ FEATURE: adicionado arquivos iniciais

---
+++ src/test/java/br/com/zeroesjobs/ZeroesjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zeroesjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroesjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
