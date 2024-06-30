76586b28e9e4c72e4b65ca46b0545a41b0e5fab2
Kelvin Hey <kelvinhey@outlook.com>
2024-06-30 16:27:06 -0300

    ✨ FEATURE: adicionado classe CandidatoSalvo

---
+++ src/main/java/br/com/zeroesjobs/entity/CandidatoSalvo.java
@@ -0,0 +1,66 @@
+package br.com.zeroesjobs.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "vaga"})})
+public class CandidatoSalvo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuarioId", referencedColumnName = "usuario_conta_id")
+    private Candidato usuarioId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "vaga", referencedColumnName = "vaga_id")
+    private Vaga vaga;
+
+    /*** CONSTRUTOR VAZIO ***/
+    public CandidatoSalvo() {
+
+    }
+
+    /*** CONSTRUTOR PADRÃO ***/
+    public CandidatoSalvo(Integer id, Candidato usuarioId, Vaga vaga) {
+        this.id = id;
+        this.usuarioId = usuarioId;
+        this.vaga = vaga;
+    }
+
+    /*** GETTERS / SETTERS ***/
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Candidato getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Candidato usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Vaga getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(Vaga vaga) {
+        this.vaga = vaga;
+    }
+
+    @Override
+    public String toString() {
+        return "CandidatoSalvo{" +
+                "id=" + id +
+                ", usuarioId=" + usuarioId +
+                ", vaga=" + vaga +
+                '}';
+    }
+}
