4886919d44da92ad756c2a64e9792a2332d15802
Kelvin Hey <kelvinhey@outlook.com>
2024-08-13 22:49:09 -0300

    ✨ FEATURE: renomeado classe ZeroesJobsApplication

--- src/main/java/br/com/zeroesjobs/ZeroesJobsApplication.java
+++ src/main/java/br/com/zeroesjobs/ZeroesJobsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZeroesjobsApplication {
+public class ZeroesJobsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZeroesjobsApplication.class, args);
+		SpringApplication.run(ZeroesJobsApplication.class, args);
 	}
 
 }
