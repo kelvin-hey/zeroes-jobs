3eb51c14dffa2715b29ad02ba26f2d97c58a1cb2
Kelvin Hey <kelvinhey@outlook.com>
2024-09-12 22:58:05 -0300

    ✨ FEATURE: adicionado classe RecrutadorService

---
+++ src/main/java/br/com/zeroesjobs/services/RecrutadorService.java
@@ -0,0 +1,23 @@
+package br.com.zeroesjobs.services;
+
+import br.com.zeroesjobs.entity.Recrutador;
+import br.com.zeroesjobs.repository.RecrutadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecrutadorService {
+
+    private final RecrutadorRepository recrutadorRepository;
+
+    @Autowired
+    public RecrutadorService(RecrutadorRepository recrutadorRepository) {
+        this.recrutadorRepository = recrutadorRepository;
+    }
+
+    public Optional<Recrutador> obter(Integer id) {
+        return recrutadorRepository.findById(id);
+    }
+}
