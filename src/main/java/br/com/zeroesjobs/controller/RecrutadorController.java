bce84d833ed1c592918d0a36efbecee030f29741
Kelvin Hey <kelvinhey@outlook.com>
2024-09-12 22:25:40 -0300

    ✨ FEATURE: adicionado classe RecrutadorController

---
+++ src/main/java/br/com/zeroesjobs/controller/RecrutadorController.java
@@ -0,0 +1,9 @@
+package br.com.zeroesjobs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/recrutador")
+public class RecrutadorController {
+}
